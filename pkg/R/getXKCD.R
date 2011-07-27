7a58151353df8305bbdc5f764a6f297cdb02aa1f
onertipaday (Paolo Sonego) <paolo.sonego@gmail.com>
2011-07-27 13:10:22 +0000

    browser visualization added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rxkcd@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getXKCD.R
+++ pkg/R/getXKCD.R
@@ -23,21 +23,6 @@ xkcd.env <- new.env()
   assign("xkcd.data", read.xkcd(), envir = xkcd.env)
 }
 
-# getXKCD <- function(which="current", display=TRUE, useGTK=FALSE, saveImg=FALSE){
-# 	require(EBImage)
-# 	if (which=="current") xkcd <- fromJSON("http://xkcd.com/info.0.json")
-# 	else if(which=="random"|which=="") {
-# 		current <- fromJSON("http://xkcd.com/info.0.json")
-# 		num = sample(1:current$num, 1)
-# 		xkcd <- fromJSON(paste("http://xkcd.com/",num,"/info.0.json",sep=""))
-# 	} 
-# 	else xkcd <- fromJSON(paste("http://xkcd.com/",which,"/info.0.json",sep=""))
-# 	xkcd.img <-readImage(xkcd$img[[1]])
-# 	if(display) display(xkcd.img,useGTK=useGTK,title=xkcd$title)
-# 	if(saveImg) writeImage(xkcd.img,paste(xkcd$title,".png",sep=""))
-# 	return(xkcd)
-# }
-
 searchXKCD<- function(which="significant", xkcd.data=NULL){
 	if(is.null(xkcd.data))
 		xkcd.data <- get("xkcd.data", envir = xkcd.env)
@@ -51,7 +36,7 @@ searchXKCD<- function(which="significant", xkcd.data=NULL){
 	return(out)	
 }
 
-getXKCD <- function(which="current", display=TRUE, saveImg=FALSE){
+getXKCD <- function(which="current", display=TRUE, html=FALSE, saveImg=FALSE) {
 	if (which=="current") xkcd <- fromJSON("http://xkcd.com/info.0.json")
 	else if(which=="random"|which=="") {
 		current <- fromJSON("http://xkcd.com/info.0.json")
@@ -59,21 +44,25 @@ getXKCD <- function(which="current", display=TRUE, saveImg=FALSE){
 		xkcd <- fromJSON(paste("http://xkcd.com/",num,"/info.0.json",sep=""))
 	} 
 	else xkcd <- fromJSON(paste("http://xkcd.com/",which,"/info.0.json",sep=""))
-	if(grepl(".png",xkcd$img)){
-		# download.file(url=xkcd$img, destfile=paste(tempdir(),"xkcd.png",sep="/"))
-		# xkcd.img <- readPNG( paste(tempdir(),"xkcd.png",sep="/") 
-		download.file(url=xkcd$img, quiet=TRUE, mode="wb", destfile=paste(tempdir(),"xkcd.png",sep="/"))
-		xkcd.img <- readPNG( paste(tempdir(),"xkcd.png",sep="/") )
+	
+	if(html) browseURL( paste("http://xkcd.com/", xkcd$num,sep="") ) 
+	else {
+		if(grepl(".png",xkcd$img)){
+			# download.file(url=xkcd$img, destfile=paste(tempdir(),"xkcd.png",sep="/"))
+			# xkcd.img <- readPNG( paste(tempdir(),"xkcd.png",sep="/") 
+			download.file(url=xkcd$img, quiet=TRUE, mode="wb", destfile=paste(tempdir(),"xkcd.png",sep="/"))
+			xkcd.img <- readPNG( paste(tempdir(),"xkcd.png",sep="/") )
+		}
+		else if(grepl(".jpg",xkcd$img)){
+			# download.file(url=xkcd$img, destfile=paste(tempdir(),"xkcd.jpg",sep="/"))
+			# xkcd.img <- read.jpeg( paste(tempdir(),"xkcd.jpg",sep="/") )
+			download.file(url=xkcd$img, quiet=TRUE, mode="wb", destfile=paste(tempdir(),"xkcd.jpg",sep="/"))
+			xkcd.img <- read.jpeg( paste(tempdir(),"xkcd.jpg",sep="/") )
+		} else stop("Unsupported image format!")
+		max.dim = max(dim(xkcd.img))
+		plot(1:max.dim, type="n", axes=F, xaxt="n",yaxt="n",xlab="",ylab="")
+		if(display) rasterImage(xkcd.img, xleft=0, ybottom=0, xright=dim(xkcd.img)[[2]], ytop=dim(xkcd.img)[[1]])
+		if(saveImg) writePNG( image=xkcd.img, target=paste(xkcd$title,".png",sep="") )
 	}
-	else if(grepl(".jpg",xkcd$img)){
-		# download.file(url=xkcd$img, destfile=paste(tempdir(),"xkcd.jpg",sep="/"))
-		# xkcd.img <- read.jpeg( paste(tempdir(),"xkcd.jpg",sep="/") )
-		download.file(url=xkcd$img, quiet=TRUE, mode="wb", destfile=paste(tempdir(),"xkcd.jpg",sep="/"))
-		xkcd.img <- read.jpeg( paste(tempdir(),"xkcd.jpg",sep="/") )
-	} else stop("Unsupported image format!")
-	max.dim = max(dim(xkcd.img))
-	plot(1:max.dim, type="n", axes=F, xaxt="n",yaxt="n",xlab="",ylab="")
-	if(display) rasterImage(xkcd.img, xleft=0, ybottom=0, xright=dim(xkcd.img)[[2]], ytop=dim(xkcd.img)[[1]])
-	if(saveImg) writePNG( image=xkcd.img, target=paste(xkcd$title,".png",sep="") )
 	return(xkcd)
 }
